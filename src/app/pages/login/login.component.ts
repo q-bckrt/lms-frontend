409d32dbf0fbc2f4556d79b0819d3f039b31a9b9
adamkorompai <korompai.ad@gmail.com>
2025-06-03 13:48:42 +0200

    login and register page created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from '../../components/navbar/navbar.component';
+import {ButtonComponent} from '../../components/button/button.component';
+import {FooterComponent} from '../../components/footer/footer.component';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NavbarComponent, ButtonComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "../../components/button/button.component";
+import {FooterComponent} from "../../components/footer/footer.component";
+import {NavbarComponent} from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+    imports: [
+        ButtonComponent,
+        FooterComponent,
+        NavbarComponent
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
