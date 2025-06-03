7e86c28a11964aa96601ae1ffbd093f371c4cc9b
adamkorompai <korompai.ad@gmail.com>
2025-06-03 15:41:29 +0200

    created the profile page + moved button to the right

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import {ButtonComponent} from '../../components/button/button.component';
+import {FooterComponent} from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ButtonComponent, FooterComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
