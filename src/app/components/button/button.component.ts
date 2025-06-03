e594429ab203b760bccd62ce19baa45b552173d6
Andrea Basili <andrea.basili@live.it>
2025-06-03 11:20:48 +0200

    homepage setup

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+  @Input() text = ''
 }
