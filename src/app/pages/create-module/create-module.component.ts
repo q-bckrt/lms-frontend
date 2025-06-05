7d378c460c6c60bfe4d9ab89a3c38622673bd02b
Andrea Basili <andrea.basili@live.it>
2025-06-05 14:39:50 +0200

    create course html

--- src/app/pages/create-module/create-module.component.ts
+++ src/app/pages/create-module/create-module.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-create-module',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, ButtonComponent],
   templateUrl: './create-module.component.html',
   styleUrl: './create-module.component.css'
 })
 export class CreateModuleComponent {
-
+  handleCreate() {}
 }
