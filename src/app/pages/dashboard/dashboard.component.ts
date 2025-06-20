f34ea4e6b21d7a2c4bb08ac32db22c38967e5c2c
q-bckrt <pom.schc@egmail.com>
2025-06-20 15:59:38 +0200

    started implementation of new dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,31 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ButtonComponent } from '../../components/button/button.component';
-import { NgIf } from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import { KeycloakServiceService } from '../../services/keycloak/keycloak-service.service';
 import { UserService } from '../../services/user-service.service';
 import { Router } from '@angular/router';
 import { RoleService } from '../../services/role-service.service';
+import {CourseService} from '../../services/course.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent, FooterComponent, ButtonComponent, NgIf ],
+  imports: [NavbarComponent, FooterComponent, ButtonComponent, NgIf, NgForOf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
   userRole = '';
   username = '';
+  courses: Array<{ id: number; title: string }> = [];
+
 
   constructor(
     private keycloakService: KeycloakServiceService,
     private userService: UserService,
     private router: Router,
-    public roleService: RoleService
+    public roleService: RoleService,
+    private courseService: CourseService
   ) {}
 
   ngOnInit() {
     this.initializeUserData();
+    this.courseService.getAllCourses().subscribe({
+      next: (courses) => {
+        this.courses = courses;
+      },
+      error: (err) => {
+        console.error('Failed to load courses:', err);
+        alert('Failed to load courses. Please try again later.');
+      }
+    })
   }
 
   private initializeUserData() {
