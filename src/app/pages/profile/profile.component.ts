d4c5891196ce30ed73f1f5b0523f33962c4751c2
Andrea Basili <andrea.basili@live.it>
2025-06-20 14:18:49 +0200

    temp implementation of assign classes

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,32 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import {ButtonComponent} from '../../components/button/button.component';
-import {FooterComponent} from '../../components/footer/footer.component';
-import {Router} from '@angular/router';
-import {UserService} from '../../services/user-service.service';
-import {KeycloakServiceService} from '../../services/keycloak/keycloak-service.service';
+import { ButtonComponent } from '../../components/button/button.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { UserService } from '../../services/user-service.service';
+import { KeycloakServiceService } from '../../services/keycloak/keycloak-service.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [NavbarComponent, ButtonComponent, FooterComponent],
+  standalone: true,
+  imports: [
+    FormsModule,
+    NavbarComponent,
+    ButtonComponent,
+    FooterComponent
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
-
   username: string = '';
   displayName: string = '';
   email: string = '';
   classes: any[] = [];
 
+  availableClasses: any[] = [];
+  selectedClassId: number | null = null;
+
   constructor(
-    private userService: UserService, 
+    private userService: UserService,
     private router: Router,
     private keycloakService: KeycloakServiceService
   ) {}
 
   ngOnInit() {
-    // Get the token from session storage
     const token = this.keycloakService.getToken();
     if (!token) {
       console.error('No token found');
@@ -34,27 +43,52 @@ export class ProfileComponent implements OnInit {
       return;
     }
 
-    // Decode the JWT token to get user information
     const tokenPayload = JSON.parse(atob(token.split('.')[1]));
     const username = tokenPayload.preferred_username;
-
     if (!username) {
       console.error('No username found in token');
       return;
     }
 
+    this.username = username;
+
     this.userService.getUserProfile(username).subscribe({
       next: (user) => {
-        console.log('User response:', user);
         this.username = user.userName;
         this.displayName = user.displayName;
         this.email = user.email;
         this.classes = user.classes;
+
+        this.selectedClassId = user.classes?.[0]?.id || null;
       },
       error: (err) => {
         console.error('Failed to load user profile:', err);
       }
     });
+
+    this.userService.getAllClasses().subscribe({
+      next: (classes) => {
+        console.log('Fetched classes:', classes);
+        this.availableClasses = classes;
+      },
+      error: (err) => {
+        console.error('Failed to load class list:', err);
+      }
+    });
+  }
+
+  onSaveClassAssignment() {
+    if (this.selectedClassId !== null) {
+      this.userService.assignClassToUser(this.username, this.selectedClassId).subscribe({
+        next: (res) => {
+          console.log('Class assigned:', res);
+          this.classes = res.classes;
+        },
+        error: (err) => {
+          console.error('Failed to assign class:', err);
+        }
+      });
+    }
   }
 
   goToEdit() {
