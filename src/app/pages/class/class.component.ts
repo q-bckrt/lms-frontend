b62e565e86b4284b2317cd3fb3194abcb6a186fb
nicolas bluj <nicolas_bluj@acslworld.com>
2025-06-23 10:50:21 +0200

    added dropdown button and installed npm bootstrap

--- src/app/pages/class/class.component.ts
+++ src/app/pages/class/class.component.ts
@@ -8,6 +8,7 @@
   import {ActivatedRoute, Router} from '@angular/router';
   import {ButtonComponent} from '../../components/button/button.component';
   import {RoleService} from '../../services/role-service.service';
+  import {ClassService} from '../../services/class-service';
 
   @Component({
     selector: 'app-class',
@@ -23,15 +24,19 @@
   })
   export class ClassComponent implements OnInit{
     private router = inject(Router);
+    private classService = inject(ClassService);
     roleService = inject(RoleService);
-    userService = inject(UserService);
-    keycloakService = inject(KeycloakServiceService);
+    private userService = inject(UserService);
+    private keycloakService = inject(KeycloakServiceService);
     private route = inject(ActivatedRoute);
 
     classId: number = 0;
     classOverviews: classOverviewModel[] = [];
     selectedClassOverview: classOverviewModel | null = null;
+
+
     loading: boolean = true;
+
 
     ngOnInit() {
       const userName = this.keycloakService.getTokenUserName()
@@ -48,13 +53,12 @@
         error: (err) => {
           console.error('Failed to fetch class overview list:', err);
           this.loading = false;
-
         }
       });
 
     }
 
-    //the argument for this function is gonna come from the route
+    //the classId for this function is gonna come from the route
     private selectClass(classId: number): void {
       this.selectedClassOverview = this.classOverviews.find(c => c.id === classId) ?? null;
     }
@@ -62,6 +66,17 @@
     goTo(path: string) {
       console.log('Button clicked');
       this.router.navigate([path]);
+    }
+
+    //the classId for this function is gonna come from the route
+    linkCourse(courseId: number) {
+      if(this.selectedClassOverview){
+        this.classService.linkCourseToClass(this.selectedClassOverview?.id,courseId)
+        console.log(`class with id ${this.selectedClassOverview.id} and title ${this.selectedClassOverview.title} linked to course with id ${courseId}`)
+      } else {
+        console.error("No class selected or selectedClassOverview = null")
+      }
+
     }
 
   }
