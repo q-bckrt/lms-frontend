9ff811018f2f6c808550fcb9a06ef5ac14b6eb8a
q-bckrt <pom.schc@egmail.com>
2025-06-26 14:27:38 +0200

    to keyclock too :)

--- src/app/services/keycloak/keycloak.interceptor.ts
+++ src/app/services/keycloak/keycloak.interceptor.ts
@@ -3,13 +3,16 @@ import {inject} from '@angular/core';
 import {KeycloakServiceService} from './keycloak-service.service';
 
 export const keycloakInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const baseUrl = process.env['BASE_URL'];
+
   // Skip interceptor for Keycloak login endpoint
   if (req.url.includes('keycloak.switchfully.com/realms/java-2025-03/protocol/openid-connect/token')) {
     return next(req);
   }
 
   // Skip interceptor for registration endpoint (POST to /users)
-  if (req.url.includes('http://localhost:8080/users') && req.method === 'POST') {
+  if (req.url.includes(`${baseUrl}` + '/users') && req.method === 'POST') {
     return next(req);
   }
 
